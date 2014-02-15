968a937fc9c8f5e8aa1220419764b04f31f82955
David Harwood <dharwood@cecs.pdx.edu>
2014-02-14 16:30:54 -0800

    add commands for quit, join, part

--- achievebot.py
+++ achievebot.py
@@ -3,7 +3,7 @@ from twisted.internet import ssl, reactor, protocol
 from twisted.words.protocols import irc
 
 #system imports
-import time, sys, argparse
+import time, sys, argparse, csv
 from ConfigParser import RawConfigParser
 
 class AchievementHandler:
@@ -13,6 +13,10 @@ class AchievementHandler:
 
     def __init__(self, config):
         #TODO: read config file, make settings changes as needed
+        pass
+
+    def command(self, user, channel, msg):
+        #TODO: do application-specific command
         pass
 
     def grant(self, user, achievement):
@@ -48,6 +52,22 @@ class AchieveBot(irc.IRCClient):
         irc.IRCClient.connectionLost(self, reason)
         self.achieve.close()
 
+    def privmsg(self, user, channel, msg):
+        if channel == self.nickname:
+            self.command(user, channel, msg.split())
+        elif msg.startswith(self.nickname):
+            self.command(user, channel, msg.split()[1:])
+
+    def command(self, user, channel, msg):
+        if msg[0] == "quit":
+            self.quit(message="I have been told to leave")
+        elif msg[0] == "join":
+            self.join(msg[1])
+        elif msg[0] == "leave":
+            self.leave(msg[1], reason="I've been told to part")
+        else:
+            self.msg(channel, self.achieve.command(msg))
+
 class AchieveBotFactory(protocol.ClientFactory):
     """
     A factory for AchieveBots.
@@ -59,14 +79,15 @@ class AchieveBotFactory(protocol.ClientFactory):
         self.config = config
 
     def clientConnectionLost(self, connector, reason):
+        reactor.stop()
         #reconnect if connection lost
-        connector.connect()
+        #connector.connect()
+
 
     def clientConnectionFailed(self, connector, reason):
         reactor.stop()
 
 if __name__ == '__main__':
-    #TODO: get stuff working here
     parser = argparse.ArgumentParser(description='Bot for IRC achievements')
     parser.add_argument('-c', '--config', default='abot.conf', metavar='FILE', help='Config file to use. If it is missing, a default configuration file will be generated at the same path.')
     parser.add_argument('-s', '--server', help='Server to connect to (overrides config file)')
